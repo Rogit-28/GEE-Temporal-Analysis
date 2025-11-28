8bb95f169d2f5bded7829462209701030fc02bb0
Rogit-28 <rogitsudharsan81@gmail.com>
2025-11-28 19:22:17 +0530

    release: v0.1.0 ready for review

--- satchange/__init__.py
+++ satchange/__init__.py
@@ -5,7 +5,7 @@ A Python-based CLI tool that enables users to detect and visualize temporal
 changes in satellite imagery for specified geographic areas using Google Earth Engine.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.0"  # Release candidate
 __author__ = "SatChange Team"
 __email__ = "team@satchange.dev"
 
